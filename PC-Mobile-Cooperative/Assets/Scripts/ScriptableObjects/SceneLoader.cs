78e8196312fe65317035e734fa521707be82a56d
Rodrigo Souza <rodrigo_souza174@hotmail.com>
2022-12-17 12:47:50 -0300

    Initial configs

---
+++ PC-Mobile-Cooperative/Assets/Scripts/ScriptableObjects/SceneLoader.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/Scene Loader")]
+public class SceneLoader : ScriptableObject
+{
+    public void LoadScene()
+    {
+
+    }
+}
